237da35a0d4f3030532b4635adc08a07b209f941
Joseph Schmitt <codewaggle@gmail.com>
2023-03-10 10:28:45 -0500

    looping

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(nameArray, giftType) {
+    let messageArray = [];
+
+    for(let i = 0; i < nameArray.length; i++) {
+        let messageString = `Thank you, ${nameArray[i]}, for the wonderful ${giftType} gift!`;
+        messageArray.push(messageString);
+
+    }
+    
+    return messageArray;
+}
+
+
+function countDown(numberIn) {
+    let counter = Number(numberIn);
+    
+    while (counter >= 0) {
+        console.log(counter);
+
+        counter--;
+    }
+}
